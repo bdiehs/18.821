fda67e844a4ab51b7e12e2076d89bfcfddb36538
David A Bau <abau@mit.edu>
2019-09-17 12:23:37 -0400

    Things are a bit of a mess right now because there was a bug

--- spanning.py
+++ spanning.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.linalg
 
 EPSILON = 1e-9
 def equalish(x, y):
@@ -7,10 +8,16 @@ def equalish(x, y):
 def first_nonzero(x):
     return next((i for i, k in enumerate(x) if k != 0), len(x))
 
+def singleton(n, i, j):
+    initial_array = [[0 for _ in range(n)] for __ in range(n)]
+    initial_array[i][j] = 1
+    return np.array(initial_array)
+
 class SpanningSet:
-    def __init__(self, initial_bases):
+    def __init__(self, initial_bases, initial_strings):
         self.basis = [x.flatten() for x in initial_bases]
         self.set = initial_bases
+        self.strings = initial_strings
         self.pivot_indices = []
 
         self._row_echelon()
@@ -23,11 +30,12 @@ class SpanningSet:
     Put our own basis in row-echelon form.
     '''
     def _row_echelon(self):
+        '''
         old_basis = self.basis
         ut_basis = []
 
         while len(old_basis) > 0:
-            old_basis = sorted(old_basis, key = first_nonzero)
+            old_basis = list(sorted(old_basis, key = first_nonzero))
 
             pivot_index = first_nonzero(old_basis[0])
 
@@ -54,7 +62,36 @@ class SpanningSet:
 
         self.basis = echelon_basis
         self.pivot_indices = [first_nonzero(x) for x in self.basis]
+        '''
 
+        _, __, self.basis = scipy.linalg.lu(np.array(self.basis))
+
+        pivots = [first_nonzero(x) for x in self.basis]
+        self.pivot_indices = pivots
+
+        for i in range(len(self.basis)):
+            self.basis[i] /= self.basis[i][pivots[i]]
+
+        for i in reversed(range(len(self.basis))):
+            for j in reversed(range(i)):
+                self.basis[j] -= self.basis[i] * self.basis[j][pivots[i]]
+
+        self.basis = list(self.basis)
+
+    def terms(self, expr):
+        if not self.full_rank():
+            raise Exception("I'm singular!")
+
+        M = np.transpose(np.array([x.flatten() for x in self.set]))
+        print(M)
+        print(np.linalg.det(M))
+        #print(M)
+        Minv = np.linalg.inv(M)
+
+        coeffs = Minv @ expr.flatten()
+
+        return ' + '.join(['(%f %s)' % (coeffs[i], self.strings[i]) for i in range(len(coeffs)) if coeffs[i] != 0])
+        #return ' + '.join(['(%f %r)' % (coeffs[i], self.set[i]) for i in range(len(coeffs)) if coeffs[i] != 0])
 
     def spans(self, candidate):
         candidate = candidate.flatten()
@@ -66,9 +103,10 @@ class SpanningSet:
         else:
             return False
 
-    def add(self, candidate):
+    def add(self, candidate, string):
         self.basis.append(candidate.flatten())
         self.set.append(candidate)
+        self.strings.append(string)
         self._row_echelon()
 
     def full_rank(self):
@@ -77,25 +115,26 @@ class SpanningSet:
 '''
 Some tests of SpanningSet
 '''
-'''
-DIM = 2
-x = SpanningSet([np.eye(DIM)])
+DIM = 5
+x = SpanningSet([np.eye(DIM)], ['I'])
 print(x.basis)
 
-y, z, w = [np.random.rand(DIM, DIM) for _ in range(3)]
-print(y)
-x.add(y)
-print(x.basis)
-print(z)
-x.add(z)
-print(x.basis)
-print(w)
-x.add(w)
-print(x.basis)
-'''
+l = [np.random.rand(DIM, DIM) for _ in range(100)]
+
+for i, y in enumerate(l):
+    print(y)
+    print(x.spans(y))
+    if not x.spans(y):
+        x.add(y, 'y%d' % i)
+        print(x.basis)
+    print('------')
+
+for z in l:
+    print(x.spans(z))
+    print(x.terms(z))
 
 def test_generation(X, Y):
-    current_spanning_set = SpanningSet([np.eye(X.shape[0])])
+    current_spanning_set = SpanningSet([np.eye(X.shape[0])], ['I'])
 
     mult_strings = ['']
     candidates = []
@@ -105,11 +144,17 @@ def test_generation(X, Y):
         new_candidates = [X @ z for z in current_spanning_set] + [Y @ z for z in current_spanning_set]
         new_candidate_strings = ['X' + z for z in mult_strings] + ['Y' + z for z in mult_strings]
 
+        print(len(new_candidate_strings))
+
         any_new = False
         for i, candidate in enumerate(new_candidates):
             if not current_spanning_set.spans(candidate):
+                print('ADDING')
+                print(new_candidate_strings[i])
+                print(len(mult_strings))
+                print(len(set(mult_strings)))
 
-                current_spanning_set.add(candidate)
+                current_spanning_set.add(candidate, new_candidate_strings[i])
                 mult_strings.append(new_candidate_strings[i])
                 candidates.append(candidate)
 
@@ -121,7 +166,7 @@ def test_generation(X, Y):
         degree += 1
 
     if current_spanning_set.full_rank():
-        return degree, mult_strings, candidates
+        return degree, current_spanning_set
     else:
         return False
 
@@ -134,10 +179,37 @@ def cyclic_shift_dimension(n):
 def diverse_diagonal_dimension(n):
     initial_array = [[0 for _ in range(n)] for __ in range(n)]
     for i in range(n):
-        initial_array[i][i] = i
+        initial_array[i][i] = i + 1
     return np.array(initial_array)
 
-for n in range(1, 20):
-    d, m, c = test_generation(cyclic_shift_dimension(n), diverse_diagonal_dimension(n))
+'''
+for n in range(1, ):
+    X = np.random.rand
+    d, S = test_generation(cyclic_shift_dimension(n), diverse_diagonal_dimension(n))
 
     print('DIMENSION %d, DEGREE %d' % (n, d))
+    print('BECAUSE:')
+    for i in range(n):
+        for j in range(n):
+            X = singleton(n, i, j) * 6
+            print('%r = %s' % (X, S.terms(X)))
+    #for mi, ci in zip(m, c):
+    #    print('%s = %r' % (mi, ci))
+'''
+
+ITERS = 20
+n = 10
+for _ in range(ITERS):
+    X, Y = diverse_diagonal_dimension(n), np.round(10 * np.random.rand(n, n))
+
+    print('X = %r' % X)
+    print('Y = %r' % Y)
+
+    d, S = test_generation(X, Y)
+
+    print('DEGREE %d' % d)
+    print('BECAUSE:')
+    for i in range(n):
+        for j in range(n):
+            X = singleton(n, i, j)
+            print('%r = %s' % (X, S.terms(X)))
